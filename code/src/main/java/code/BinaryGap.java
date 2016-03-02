67a59fab20089ad87344feb074b9806ae7b8656b
Gustavo Dobro <mgdobro@gmail.com>
2016-03-01 23:29:18 -0300

    binary gap

---
+++ code/src/main/java/code/BinaryGap.java
@@ -0,0 +1,25 @@
+package code;
+
+public class BinaryGap {
+
+	public int calculate(int N) {
+        String binary = Integer.toBinaryString(N);
+        int i = binary.length()-1;
+        while(binary.charAt(i)=='0') {
+            i--;
+        }
+        int gap = 0;
+        int counter = 0;
+        for(; i>=0; i--) {
+            if(binary.charAt(i)=='1') {
+                gap = Math.max(gap, counter);
+                counter = 0;
+            } else {
+                counter++;
+            }
+        }
+        gap = Math.max(gap, counter);
+        return gap;
+	}
+
+}
---
+++ code/src/test/java/code/BinaryGapTest.java
@@ -0,0 +1,16 @@
+package code;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryGapTest {
+
+	@Test
+	public void test() {
+		BinaryGap bg = new BinaryGap();
+		
+		assertEquals(1, bg.calculate(145));
+	}
+
+}
